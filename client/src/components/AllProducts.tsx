9c5d3c3d786a7b6b8a5e38e4f4a1a38d141d0dd9
ajayupadhyay111 <upadhyayajay156@gmail.com>
2025-04-21 18:51:24 +0530

    all product page completed

---
+++ client/src/components/AllProducts.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useAppContext } from "../context/appContext";
+import { Product } from "../types";
+import ProductCard from "./ProductCard";
+
+const AllProducts = () => {
+  const { products, searchQuery } = useAppContext();
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+  useEffect(() => {
+    setFilteredProducts(
+      products.filter((product) =>
+        product.name.toLowerCase().includes(searchQuery || "")
+      )
+    );
+  }, [products, searchQuery]);
+  return (
+    <div className="mt-16 flex flex-col">
+      <div className=" flex flex-col items-end w-max">
+        <p className="text-2xl font-medium uppercase">All products</p>
+        <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+          </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-6">
+        {
+            filteredProducts.length > 0 ?
+            filteredProducts.map((product) => (
+                <ProductCard key={product._id} product={product} />
+            ))
+        :"No products"}
+      </div>
+    </div>
+  );
+};
+
+export default AllProducts;
