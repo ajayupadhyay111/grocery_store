570fb633def2861bf5f34a6e90bcaa2a80920ec2
ajayupadhyay111 <upadhyayajay156@gmail.com>
2025-04-29 23:54:17 +0530

    completed

--- server/src/index.js
+++ server/src/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://grocery-store-silk-iota.vercel.app",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
